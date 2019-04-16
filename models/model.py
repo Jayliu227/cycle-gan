65ef940e44677bf1a13ab47f0df4898ad54f2881
Jingyu Liu <jingyuliu@10-16-90-158.dynapool.wireless.nyu.edu>
2019-04-16 09:28:41 -0400

    add model folder

---
+++ models/model.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+
+class ResBlock(nn.Module):
+    pass
+
+class G(nn.Module):
+    pass
+
+class F(nn.Module):
+    pass
+
+class Dx(nn.Module):
+    pass
+
+class Dy(nn.Module):
+    pass
