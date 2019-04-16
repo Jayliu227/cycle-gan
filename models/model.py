17e035421c1919c7a1ad6a939c89a06b33a8e880
Jingyu Liu <jingyuliu@10-16-90-158.dynapool.wireless.nyu.edu>
2019-04-16 19:26:40 -0400

    Added generator and resblock

--- models/model.py
+++ models/model.py
@@ -10,10 +10,60 @@ import torchvision.utils as vutils
 import numpy as np
 
 class ResBlock(nn.Module):
-    pass
+    def __init__(self, num_channels):
+        super(ResBlock, self).__init__()
+        model = [
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(num_channels, num_channels, 3), 
+            nn.BatchNorm2d(num_channels),
+            nn.ReLU(inplace=True),
+            
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(num_channels, num_channels, 3),
+            nn.BatchNorm2d(num_channels),
+            nn.ReLU(inplace=True),
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(num_channels, num_channels, 3), 
+            nn.BatchNorm2d(num_channels)
+        ]
+        self.model = nn.Sequential(*model)
+   
+    def forward(self, x):
+        identity = x
+        out = self.model(x)
+        out += identity
 
 class Generator(nn.Module):
     pass
 
 class Discriminator(nn.Module):
-    pass
+    def __init__(self, num_channels):
+        super(Discriminator, self).__init__()
+        model = [
+            nn.Conv2d(num_channels, 10, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+        ]
+        model += [
+            nn.Conv2d(10, 20, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(20),
+            nn.LeakyReLU(0.2, inplace=True)
+        ]
+        model += [
+            nn.Conv2d(20, 40, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(40),
+            nn.LeakyReLU(0.2, inplace=True)
+        ]
+        model += [
+            nn.Conv2d(40, 80, 4, 1, 1, bias=False),
+            nn.BatchNorm2d(80),
+            nn.LeakyReLU(0.2, inplace=True)
+        ]
+        model += [
+            nn.Conv2d(80, 1, 4, 1, 1, bias=False),
+        ]
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        out = self.model(x)
+        return out
