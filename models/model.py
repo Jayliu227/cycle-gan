680db4ec78d94215d7e09f21f04d2d47f5af0495
RuizhenMai <mairuizhen1998@gmail.com>
2019-04-19 20:21:50 -0400

    finish unet  generator

--- models/model.py
+++ models/model.py
@@ -98,7 +98,7 @@ class UnetBlock(nn.Module):
 
             down = [downrelu, downconv] # Todo - i think it's ok to add batchnorm?
             up = [uprelu, upconv, upnorm]
-            mdoel = down + up
+            model = down + up
 
         else:
             down = [downrelu, downconv, downnorm] 
@@ -110,14 +110,14 @@ class UnetBlock(nn.Module):
     def forward(self, x):
         if self.outermost: # no residual input
             return self.model(x)
-
+        
         # input image is arranged as 
         # numOfTotalImages * num_channels * Witdh * Height 
         return torch.cat([x, self.model(x)], 1)       
 
 
 
-
+# Generator 
 class Unet(nn.Module):
     def __init__(self, input_nc, output_nc, ngf = 64):
         """Construct a Unet by UnetBlock
@@ -137,7 +137,7 @@ class Unet(nn.Module):
         unet_block = UnetBlock(ngf * 2, ngf * 4, submodule = unet_block)
         unet_block = UnetBlock(ngf * 1, ngf * 2, submodule = unet_block)        
         # assuming output_nc = input_nc 
-        unet_block = UnetBlock(input_nc, ngf, submodule = unet_block, outermost = True)      
+        self.model = UnetBlock(input_nc, ngf, submodule = unet_block, outermost = True)      
 
     def forward(self, image):
         return self.model(image)
@@ -173,72 +173,3 @@ class Discriminator(nn.Module):
         out = self.model(x)
         return out
 
-
-
-class UnetSkipConnectionBlock(nn.Module):
-    """Defines the Unet submodule with skip connection.
-        X -------------------identity----------------------
-        |-- downsampling -- |submodule| -- upsampling --|
-    """
-
-    def __init__(self, outer_nc, inner_nc, input_nc=None,
-                 submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False):
-        """Construct a Unet submodule with skip connections.
-        Parameters:
-            outer_nc (int) -- the number of filters in the outer conv layer
-            inner_nc (int) -- the number of filters in the inner conv layer
-            input_nc (int) -- the number of channels in input images/features
-            submodule (UnetSkipConnectionBlock) -- previously defined submodules
-            outermost (bool)    -- if this module is the outermost module
-            innermost (bool)    -- if this module is the innermost module
-            norm_layer          -- normalization layer
-            user_dropout (bool) -- if use dropout layers.
-        """
-        super(UnetSkipConnectionBlock, self).__init__()
-        self.outermost = outermost
-        if type(norm_layer) == functools.partial:
-            use_bias = norm_layer.func == nn.InstanceNorm2d
-        else:
-            use_bias = norm_layer == nn.InstanceNorm2d
-        if input_nc is None:
-            input_nc = outer_nc
-        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4,
-                             stride=2, padding=1, bias=use_bias) # W / 2 with this combo
-        downrelu = nn.LeakyReLU(0.2, True)
-        downnorm = norm_layer(inner_nc)
-        uprelu = nn.ReLU(True)
-        upnorm = norm_layer(outer_nc)
-
-        if outermost:
-            upconv = nn.ConvTranspose2d(inner_nc * 2, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1)
-            down = [downconv]
-            up = [uprelu, upconv, nn.Tanh()]
-            model = down + [submodule] + up
-        elif innermost:
-            upconv = nn.ConvTranspose2d(inner_nc, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1, bias=use_bias)
-            down = [downrelu, downconv]
-            up = [uprelu, upconv, upnorm]
-            model = down + up
-        else:
-            upconv = nn.ConvTranspose2d(inner_nc * 2, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1, bias=use_bias)
-            down = [downrelu, downconv, downnorm]
-            up = [uprelu, upconv, upnorm]
-
-            if use_dropout:
-                model = down + [submodule] + up + [nn.Dropout(0.5)]
-            else:
-                model = down + [submodule] + up
-
-        self.model = nn.Sequential(*model)
-
-    def forward(self, x):
-        if self.outermost:
-            return self.model(x)
-        else:   # add skip connections
-            return torch.cat([x, self.model(x)], 1)
